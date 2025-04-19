45d37fea953281acd6dfbf34ca16c100822eb481
tanvir084 <65416728+tanvir084@users.noreply.github.com>
2025-04-19 21:36:47 +0600

    fix: cowsays animal not found issue

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 sudo apt-get install cowsay -y
-cowsay -f tiger "Run for cover, I am a TIGER.....RAWR" >> tiger.txt
-grep -i "tiger" tiger.txt
-cat tiger.txt
+cowsay -f elephant "Run for cover, I am a ELEPHANT.....RAWR" >> elephant.txt
+grep -i "elephant" elephant.txt
+cat elephant.txt
 ls -ltra
